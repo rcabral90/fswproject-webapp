6bad1143a20a729c688abe49a292513b020410b9
Jeremy Chavez <kaos_x5s@yahoo.com>
2014-04-12 14:48:10 -0700

    Changes:
    	-> Most forms now post properly
    	-> initial implementation of resident selection (this is not working yet!)

--- com_fsw_webapp/views.py
+++ com_fsw_webapp/views.py
@@ -2,6 +2,7 @@ from django.core.context_processors import csrf
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 from com_fsw_service.user_authentication import login_user
+from django.shortcuts import redirect
 
 
 def home(request):
@@ -9,12 +10,23 @@ def home(request):
     context.update(csrf(request))
     return render_to_response('home.html', {"error": False}, context_instance=RequestContext(request))
 
+def selector(request):
+    if (request.session['current_resident'] == 0):
+        #render selection page
+        return render_to_response('patient_select.html', {"error": True}, context_instance=RequestContext(request))
+    if (request.method == "POST"):
+		#try to set vars
+		request.session['current_resident'] = request.POST['resident_id']
+        #push to dashboard
+        return render_to_response('dashboard.html', {"user": request.session.get('user'),"resident_id": request.session.get('current_resident')}, context_instance=RequestContext(request))
 
 def login(request):
     try:
         user = login_user(request)
         if user != "":
-            return render_to_response('dashboard.html', {"user": user}, context_instance=RequestContext(request))
+            request.session['user'] = user.username
+            request.session['current_resident'] = 0
+            return redirect('/selector/')
         else:
             return render_to_response('home.html', {"error": True}, context_instance=RequestContext(request))
     except:
---
+++ static/js/data_pushers.js
@@ -0,0 +1,151 @@
+function resetForm($form) {
+    $form.find('input:text, input:password, input:file, select, textarea').val('');
+    $form.find('input:radio, input:checkbox')
+         .removeAttr('checked').removeAttr('selected');
+}
+//form submit events
+$('#medication_entry').on("submit", function(event){
+	event.preventDefault();
+	var json = $(this).serializeJSON();
+	var resident_id = json['resident_id'];
+	json['medication_id'] = parseInt(json['medication_id']);
+	json['resident_id'] = parseInt(json['resident_id']);
+	json['med_dose_mg'] = parseInt(json['med_dose_mg']);
+	json = JSON.stringify(json);
+	$.ajax({
+		type: "POST",
+		contentType: 'application/json',
+		url: "http://127.0.0.1:7177/medication/*/",
+		data: json,
+		dataType: "json"
+	}).done(function(){
+		get_current_medication_information(resident_id);
+		//clear the form
+		resetForm($('#medication_entry'));
+		//close the form
+		$('#form_open_me').empty();
+		$('#form_open_me').append('New Entry');
+		$('#medication_entry').slideUp();
+	});
+	return false;
+})
+$('#medication_history_entry').on("submit", function(event){
+	event.preventDefault();
+	var json = $(this).serializeJSON();
+	var resident_id = json['resident_id'];
+	json['medication_id'] = parseInt(json['medication_id']);
+	json['resident_id'] = parseInt(json['resident_id']);
+	json = JSON.stringify(json);
+	$.ajax({
+		type: "POST",
+		contentType: 'application/json',
+		url: "http://127.0.0.1:7177/medicationhistory/*/",
+		data: json,
+		dataType: "json"
+	}).done(function(){
+		get_medication_history(resident_id);
+		//clear the form
+		resetForm($('#medication_history_entry'));
+		//close the form
+		$('#form_open_mhe').empty();
+		$('#form_open_mhe').append('New Entry');
+		$('#medication_history_entry').slideUp();
+	});
+	return false;
+})
+$('#prescriptions_entry').on("submit", function(event){
+	event.preventDefault();
+	var json = $(this).serializeJSON();
+	var resident_id = json['resident_id'];
+	json['medication_id'] = parseInt(json['medication_id']);
+	json['resident_id'] = parseInt(json['resident_id']);
+	json['prescription_number'] = parseInt(json['prescription_number']);
+	json = JSON.stringify(json);
+	$.ajax({
+		type: "POST",
+		contentType: 'application/json',
+		url: "http://127.0.0.1:7177/prescription/*/",
+		data: json,
+		dataType: "json"
+	}).done(function(){
+		get_prescription_information(resident_id);
+		//clear the form
+		resetForm($('#prescriptions_entry'));
+		//close the form
+		$('#form_open_pe').empty();
+		$('#form_open_pe').append('New Entry');
+		$('#prescriptions_entry').slideUp();
+	});
+	return false;
+})
+$('#assessments_entry').on("submit", function(event){
+	event.preventDefault();
+	var json = $(this).serializeJSON();
+	var resident_id = json['resident_id'];
+	json['assessment_id'] = parseInt(json['assessment_id']);
+	json['resident_id'] = parseInt(json['resident_id']);
+	json['assessment_time'] = json['assessment_time']+":00";
+	json = JSON.stringify(json);
+	$.ajax({
+		type: "POST",
+		contentType: 'application/json',
+		url: "http://127.0.0.1:7177/assessment/*/",
+		data: json,
+		dataType: "json"
+	}).done(function(){
+		get_resident_assessment_information(resident_id);
+		//clear the form
+		resetForm($('#assessments_entry'));
+		//close the form
+		$('#form_open_ae').empty();
+		$('#form_open_ae').append('New Entry');
+		$('#assessments_entry').slideUp();
+	});
+	return false;
+})
+$('#allergy_entry').on("submit", function(event){
+	event.preventDefault();
+	var json = $(this).serializeJSON();
+	var resident_id = json['resident_id'];
+	json['resident_id'] = parseInt(json['resident_id']);
+	json = JSON.stringify(json);
+	$.ajax({
+		type: "POST",
+		contentType: 'application/json',
+		url: "http://127.0.0.1:7177/allergies/*/",
+		data: json,
+		dataType: "json"
+	}).done(function(){
+		get_resident_allergy_information(resident_id);
+		//clear the form
+		resetForm($('#allergy_entry'));
+		//close the form
+		$('#form_open_ale').empty();
+		$('#form_open_ale').append('New Entry');
+		$('#allergy_entry').slideUp();
+	});
+	return false;
+})
+$('#diet_entry').on("submit", function(event){
+	event.preventDefault();
+	var json = $(this).serializeJSON();
+	var resident_id = json['resident_id'];
+	json['resident_id'] = parseInt(json['resident_id']);
+	json = JSON.stringify(json);
+	$.ajax({
+		type: "POST",
+		contentType: 'application/json',
+		url: "http://127.0.0.1:7177/diets/*/",
+		data: json,
+		dataType: "json"
+	}).done(function(){
+		get_resident_diet_information(resident_id);
+		//clear the form
+		resetForm($('#diet_entry'));
+		//close the form
+		$('#form_open_de').empty();
+		$('#form_open_de').append('New Entry');
+		$('#diet_entry').slideUp();
+	});
+	return false;
+})
