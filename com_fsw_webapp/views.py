92567e66d0b89443ef00f91991af2dac2556efe3
Jeremy Chavez <kaos_x5s@yahoo.com>
2014-04-13 12:57:02 -0700

    Comments out resident selection stuff

--- com_fsw_webapp/views.py
+++ com_fsw_webapp/views.py
@@ -15,18 +15,18 @@ def selector(request):
         #render selection page
         return render_to_response('patient_select.html', {"error": True}, context_instance=RequestContext(request))
     if (request.method == "POST"):
-		#try to set vars
-		request.session['current_resident'] = request.POST['resident_id']
+        #try to set vars
+        request.session['current_resident'] = request.POST['resident_id']
         #push to dashboard
         return render_to_response('dashboard.html', {"user": request.session.get('user'),"resident_id": request.session.get('current_resident')}, context_instance=RequestContext(request))
-
 def login(request):
     try:
         user = login_user(request)
         if user != "":
             request.session['user'] = user.username
             request.session['current_resident'] = 0
-            return redirect('/selector/')
+            #return redirect('/selector/')
+            return render_to_response('dashboard.html', {"user": user}, context_instance=RequestContext(request))
         else:
             return render_to_response('home.html', {"error": True}, context_instance=RequestContext(request))
     except:
